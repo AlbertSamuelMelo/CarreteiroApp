f9d9479d5e6f9c502d725a8299afb805b2b0f946
AlbertSamuelMelo <asm.asm.albert@icloud.com>
2020-10-24 03:57:24 -0400

    Created Criar screen
    Signed-off-by: AlbertSamuelMelo <asm.asm.albert@icloud.com>

---
+++ src/screens/CreateRegistry.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: '#6C81E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
