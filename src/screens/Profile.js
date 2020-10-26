a280564df4e1951abdff5efd1b29ee42e372a26a
AlbertSamuelMelo <asm.asm.albert@icloud.com>
2020-10-26 10:59:21 -0400

    Added Export Data Button on profile
    Signed-off-by: AlbertSamuelMelo <asm.asm.albert@icloud.com>

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import CameraPlaceHolder from '../components/CameraPlaceHolder';
 import TextPlaceHolder from '../components/TextPlaceHolder'
 
@@ -19,6 +19,11 @@ export default class Profile extends Component {
         <CameraPlaceHolder/>
         <TextPlaceHolder text="Albert"/>
         <TextPlaceHolder text="Adiministrador"/>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => this.createRegistry()}>
+              <Text style={styles.text}>Exportar Dados</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -31,5 +36,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#115B73',
     alignItems: 'center',
     justifyContent: 'flex-end',
+  },
+  buttonContainer: {
+    width: "90%",
+    height: "8%",
+    marginTop: "5%",
+    marginBottom: "8%",
+    backgroundColor: 'white',
+    borderRadius: 100,
+    shadowColor: 'black',
+    shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+  },
+  text: {
+    color: '#115B73',
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+    padding: "3.5%"
   }
 });
